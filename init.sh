e6038799bdeaf98bb75437c46888010e2c98d939
hhyasdf <552483776@qq.com>
2018-05-13 17:26:39 +0800

    update 2018/5/13

--- init.sh
+++ init.sh
@@ -5,3 +5,13 @@ sudo systemctl enable NetworkManager
 sudo systemctl enable shadowsocks@connect
 sudo systemctl enable sddm.service
 sudo systemctl enable udisks2.service
+
+
+
+
+
+#sudo systemctl disable bumblebeed.service
+#sudo systemctl disable NetworkManager
+#sudo systemctl disable shadowsocks@connect
+#sudo systemctl disable sddm.service
+#sudo systemctl disable udisks2.service
